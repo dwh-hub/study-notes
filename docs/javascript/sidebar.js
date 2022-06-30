bb0280fa3bbeef4fd670e4efd270d995ab2838e9
dahuayuan <wenhao.dong@sumwhy.com>
2022-06-30 16:50:39 +0800

    feat(javascript): HTTP协议

--- docs/javascript/sidebar.js
+++ docs/javascript/sidebar.js
@@ -2,6 +2,9 @@ module.exports = [
   {
     title: "基础",
     collapsable: true,
-    children: ["/javascript/基础/原型和原型链"],
+    children: [
+      "/javascript/基础/原型和原型链",
+      "/javascript/基础/HTTP协议",
+    ],
   },
 ];
