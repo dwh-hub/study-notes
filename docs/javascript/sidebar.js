961fa21dec9568412c896d6a0953d9b2bdb1d06f
dahuayuan <dwhandwp@163.com>
2022-07-04 23:37:28 +0800

    feat(javascript): 继承

--- docs/javascript/sidebar.js
+++ docs/javascript/sidebar.js
@@ -4,6 +4,7 @@ module.exports = [
     collapsable: true,
     children: [
       "/javascript/基础/原型和原型链",
+      "/javascript/基础/继承",
       "/javascript/基础/HTTP协议",
       "/javascript/基础/apply与call",
     ],
