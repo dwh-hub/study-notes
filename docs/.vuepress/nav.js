2a4fcb41a7a487d5ecdc9515593ab6d288a94b91
dahuayuan <dwhandwp@163.com>
2022-07-20 02:43:19 +0800

    feat(javascript): 新增一句话知识和算法

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,4 +1,5 @@
 module.exports = [
   { text: 'JavaScript', link: '/javascript/'},
   { text: 'CSS', link: '/css/'},
+  { text: '工程化', link: '/engineering/'},
 ];
--- docs/javascript/sidebar.js
+++ docs/javascript/sidebar.js
@@ -3,11 +3,13 @@ module.exports = [
     title: "基础",
     collapsable: true,
     children: [
+      "/javascript/基础/一句话知识",
       "/javascript/基础/作用域与作用域链",
       "/javascript/基础/原型和原型链",
       "/javascript/基础/继承",
       "/javascript/基础/HTTP协议",
       "/javascript/基础/apply与call",
+      "/javascript/基础/算法",
     ],
   },
   {
